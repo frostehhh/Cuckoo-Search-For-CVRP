fa3f76fcdca03365b9091fc74ab4df486ac57f8c
Luis Capuyon <33508169+frostehhh@users.noreply.github.com>
2019-10-06 16:03:51 +0800

    res

--- CuckooSearchCVRP.py
+++ CuckooSearchCVRP.py
@@ -122,25 +122,25 @@ class CuckooSearch:
         #     self.__shift1(nest)
         
         # Two Small Neighborhood
-        smallStepChoice = random.choice([1,2])
-        if smallStepChoice == 1:
-            for i in range(iterateNum):
-                self.__crossTwoOpt(nest)
-        else:
-            for i in range(iterateNum):
-                self.__exchangeIntra(nest)
+        # smallStepChoice = random.choice([1,2])
+        # if smallStepChoice == 1:
+        #     for i in range(iterateNum):
+        #         self.__swap2_1(nest)
+        # else:
+        #     for i in range(iterateNum):
+        #         self.__shift1(nest)
 
         # Two  Small Neighborhood and One Large
-        # if iterateNum <= 4:
-        #     smallStepChoice = random.choice([1,2])
-        #     if smallStepChoice == 1:
-        #         for i in range(iterateNum):
-        #             self.__crossTwoOpt(nest)
-        #     else:
-        #         for i in range(iterateNum):
-        #             self.__reinsertionIntra(nest)
-        # else:
-        #     self.__crossDoubleBridgeInter(nest)
+        if iterateNum <= 4:
+            smallStepChoice = random.choice([1,2])
+            if smallStepChoice == 1:
+                for i in range(iterateNum):
+                    self.__swap2_1(nest)
+            else:
+                for i in range(iterateNum):
+                    self.__shift1(nest)
+        else:
+            self.__crossDoubleBridgeInter(nest)
 
         # One Small Neighborhood and One Large
         # if iterateNum <= 4:
