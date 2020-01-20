e1792e9135ef1c66c5066731852236e571bf5da4
Luis Capuyon <33508169+frostehhh@users.noreply.github.com>
2020-01-20 18:18:00 +0800

    Updated wilcoxon

--- Wilcoxon.py
+++ Wilcoxon.py
@@ -7,11 +7,19 @@ fileNameSuffix = 'FinalResultsPerRun - Work'
 data = pd.read_csv(path + fileNameSuffix + '.csv', header=[0], index_col=0)
 implementationList = data.Implementation.unique()
 instanceList = data.Name.unique()
+implementationRange = range(len(implementationList))
+
+
 
 df1 = data.loc[data["Implementation"] == '2-opt, shift-1, double-bridge']
 df1_data = df1["Best Solution Cost"].tolist()
 
 print(df1.head())
-print(implementationList)
-print(df1_data)
-print(instanceList)
+print(implementationRange)
+
+
+
+for i in implementationRange:
+    for j in implementationRange[1:]:
+        # compare i and j
+        # iterate through every instance of implementations i and j
