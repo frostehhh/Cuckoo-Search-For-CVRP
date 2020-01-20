a19c6af772b7793b37844bb1cb4395a236100ee9
Luis Capuyon <33508169+frostehhh@users.noreply.github.com>
2020-01-20 18:12:49 +0800

    Updated wilcoxon.py

--- Wilcoxon.py
+++ Wilcoxon.py
@@ -2,10 +2,16 @@ from scipy.stats import wilcoxon
 import pandas as pd
 
 path = 'newFinalResults/'
-fileNameSuffix = 'FinalResultsPerRun'
+fileNameSuffix = 'FinalResultsPerRun - Work'
 
 data = pd.read_csv(path + fileNameSuffix + '.csv', header=[0], index_col=0)
+implementationList = data.Implementation.unique()
+instanceList = data.Name.unique()
 
-columns = data.columns
-df1 = data.loc[data["Implementation"] == '2-opt, double-bridge']
+df1 = data.loc[data["Implementation"] == '2-opt, shift-1, double-bridge']
+df1_data = df1["Best Solution Cost"].tolist()
+
 print(df1.head())
+print(implementationList)
+print(df1_data)
+print(instanceList)
